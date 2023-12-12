2bcdc82d25d0ad42a35954c7d8c3c7d903fada59
sukanya004sinha <sukanyasinha004@gmail.com>
2023-12-12 08:40:15 +0530

    jsx element vs component and conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 //const div = React.createElement('div',{className: 'text'},"Hello from Sukanya,I am inside react now")
 const child1 = React.createElement('p',{},"I am child2")
 const child2 = React.createElement('p',{},"I am child2")
@@ -8,11 +8,20 @@ const apiData = "hello 2 form api"
 const isMorning = false
 const morningText = "good morning text"
 const afternoonText = "Afternoon User"
-const morningElement = <div>{morningText} it is 8 am</div>
+function handleClick(state) {
+    console.log("I was clicked",state)
+}
+const morningElement = <div>{morningText} <span onClick={()=> handleClick(isMorning)}>
+it is 8 am </span></div>
 const afternoonElement = <div>{afternoonText}It is 6 pm</div>
 // const div =<div className="text">{apiData}</div>
-const mesage =<div className="text">{isMorning? morningText: afternoonText}</div>
-const div = isMorning?morningElement: afternoonElement
-console.log(div)
+// const mesage =<div className="text">{isMorning? morningText: afternoonText}</div>
+const greetingElement = isMorning?morningElement: afternoonElement
+const GreetingComponent = () => 
+<>
+ {greetingElement}
+ I am inside component 
+    </>
+console.log(GreetingComponent)
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(div)
+root.render(<GreetingComponent/>) 
